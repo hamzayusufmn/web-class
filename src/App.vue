1ee2c00c85fbd00968477d2fd83e1cf5ebd06116
hamza305 <abc@gmail.com>
2024-12-01 21:01:09 -0600

    week 10 lab done

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue';
+import WouldYouRather from './components/WouldYouRather.vue'
+// projects need to be modifed to project i am working on for this i changed it to would you rather
+
+const wyrQuestion = ref('Would you rather live in a house shaped like a triangle or a circle?');
+const wyrAnswer1 = ref('Triangle house')
+const wyrAnswer2 = ref('Circle house')
+
+// this is reactive data and asks user is they choose a circle or triangle house
+
+// this will store the user answer when they pick we dont know so we leave it on empty string
+const userSelection = ref('');
+
+function updateUserSelection(userChoice) {
+  userSelection.value = `Thanks! You chose ${userChoice}` // this will update with this message when user selects a choice
+  // whatever is in choice value will be entered here, for example it will print out thank you chose ... either circle or triangle
+
+
+}
+</script>
+
+
+
+<template>
+  <h1>Would You Rather?</h1>
+  <WouldYouRather
+      v-bind:question="wyrQuestion"
+      v-bind:answer1="wyrAnswer1"
+      v-bind:answer2="wyrAnswer2"
+      v-on:answer-selected="updateUserSelection"
+  ></WouldYouRather>
+
+  <!--when an answer is selected and changed it will update and  -->
+
+  <p>{{ userSelection }}</p>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2em;
+  margin-bottom: 20px;
+}
+
+p {
+  margin-top: 20px;
+  color: indianred;
+}
+</style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+defineProps({
+  question: String,
+  answer1: String,
+  answer2: String,
+  // this will expect answer 1 or answer 2 and asks the user for their choice
+
+
+})
+ const emit = defineEmits(['answer-selected'])
+const choice = ref('')
+// this will wait until user selects a choice between circle and triangle and
+
+// this is a child component and these consts are brought from app.vuee
+// function choiceMade() {
+ //  emit('answer-selected', choice.value)
+// }
+
+watch(choice, ()=> {
+  emit('answer-selected', choice.value)
+  // when it changes answer selected will be updated, with this additon it will remove us using v-on change
+})
+</script>
+
+<template>
+<div id="wyr"></div>
+
+  <h1> choose one please</h1>
+  <p>{{ question }}</p>
+
+<div>
+  <input v-model="choice"  v-bind:value="answer1" type="radio" id="answer1" >
+  <label for="answer1"> {{ answer1}}</label>
+
+  <input v-model="choice" v-bind:value="answer2"  type="radio" id="answer2" >
+  <label for="answer2">{{ answer2 }}</label>
+
+</div>
+<!-- we will give it values to assoicate a unique value
+ when the first is selected it will be answer 1 and the second will be answer 2 the v-model choice will allow user to select and binds -->
+
+
+
+
+
+</template>
+
+<style scoped>
+wyr {
+  background-color: mediumaquamarine;
+}
+
+label {
+margin-left: 5px;
+}
+p {
+  font-family: "Apple Symbols";
+}
+</style>
